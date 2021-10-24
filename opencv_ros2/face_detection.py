2df80b929b0a7055e617a6837623165dbd056fd0
Jeffrey Tan <i@jeffreytan.org>
2021-10-25 02:14:24 +0800

    Create face_detection.py

---
+++ opencv_ros2/face_detection.py
@@ -0,0 +1,99 @@
+# Basic ROS 2 program for Face Detection using Haar Cascades in OpenCV
+# video from your built-in webcam
+# Author:
+# - Jeffrey Tan
+# - i@jeffreytan.org
+  
+# Import the necessary libraries
+import rclpy # Python Client Library for ROS 2
+from rclpy.node import Node # Handles the creation of nodes
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2 # OpenCV library
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('/home/whale/dev_ws/src/cv_basics/cv_basics/haarcascade_frontalface_default.xml')
+ 
+class FaceDetection(Node):
+  """
+  Create an FaceDetection class, which is a subclass of the Node class.
+  """
+  def __init__(self):
+    """
+    Class constructor to set up the node
+    """
+    # Initiate the Node class's constructor and give it a name
+    super().__init__('face_detection')
+      
+    # Create the publisher. This publisher will publish an Image
+    # to the video_frames topic. The queue size is 10 messages.
+    self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
+      
+    # We will publish a message every 0.1 seconds
+    timer_period = 0.1  # seconds
+      
+    # Create the timer
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+         
+    # Create a VideoCapture object
+    # The argument '0' gets the default webcam.
+    self.cap = cv2.VideoCapture(0)
+         
+    # Used to convert between ROS and OpenCV images
+    self.br = CvBridge()
+   
+  def timer_callback(self):
+    """
+    Callback function.
+    This function gets called every 0.1 seconds.
+    """
+    # Capture frame-by-frame
+    # This method returns True/False as well
+    # as the video frame.
+    ret, frame = self.cap.read()
+          
+    if ret == True:
+      # Publish the image.
+      # The 'cv2_to_imgmsg' method converts an OpenCV
+      # image to a ROS 2 image message
+      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+ 
+    # Display the message on the console
+    ##self.get_logger().info('Publishing video frame')
+    
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow("camera", frame)
+    
+    cv2.waitKey(1)
+  
+def main(args=None):
+  
+  # Initialize the rclpy library
+  rclpy.init(args=args)
+  
+  # Create the node
+  face_detection = FaceDetection()
+  
+  # Spin the node so the callback function is called.
+  rclpy.spin(face_detection)
+  
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  face_detection.destroy_node()
+  
+  # Shutdown the ROS client library for Python
+  rclpy.shutdown()
+  
+  # Release the VideoCapture object
+  cap.release()
+  
+if __name__ == '__main__':
+  main()
