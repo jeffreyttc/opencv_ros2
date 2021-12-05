15eff5cc925f041de7f91fa9878422d9d8c4a4f8
Jeffrey Tan <i@jeffreytan.org>
2021-12-06 02:03:10 +0800

    Update face_detection.py

--- opencv_ros2/face_detection.py
+++ opencv_ros2/face_detection.py
@@ -1,18 +1,23 @@
-# Basic ROS 2 program for Face Detection using Haar Cascades in OpenCV
-# video from your built-in webcam
+# Basics ROS 2 program to subscribe to real-time streaming 
+# video from RGB-D sensor
 # Author:
-# - Jeffrey Tan
-# - i@jeffreytan.org
+# - Jeffrey
+# - https://github.com/jeffreyttc
   
 # Import the necessary libraries
-import rclpy # Python Client Library for ROS 2
+import rclpy # Python library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
 
 # Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+from rclpy.qos import qos_profile_sensor_data
  
 class FaceDetection(Node):
   """
@@ -25,51 +30,45 @@ class FaceDetection(Node):
     # Initiate the Node class's constructor and give it a name
     super().__init__('face_detection')
       
-    # Create the publisher. This publisher will publish an Image
-    # to the video_frames topic. The queue size is 10 messages.
-    self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
-      
-    # We will publish a message every 0.1 seconds
-    timer_period = 0.1  # seconds
-      
-    # Create the timer
-    self.timer = self.create_timer(timer_period, self.timer_callback)
-         
-    # Create a VideoCapture object
-    # The argument '0' gets the default webcam.
-    self.cap = cv2.VideoCapture(0)
-         
+    # Create the subscriber. This subscriber will receive an Image
+    # from the video_frames topic. The queue size is 10 messages.
+    self.subscription = self.create_subscription(
+      Image, 
+      '/image_raw',
+      self.listener_callback, 
+      qos_profile_sensor_data)
+    self.subscription # prevent unused variable warning
+
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
    
-  def timer_callback(self):
+  def listener_callback(self, data):
     """
     Callback function.
-    This function gets called every 0.1 seconds.
     """
-    # Capture frame-by-frame
-    # This method returns True/False as well
-    # as the video frame.
-    ret, frame = self.cap.read()
-          
-    if ret == True:
-      # Publish the image.
-      # The 'cv2_to_imgmsg' method converts an OpenCV
-      # image to a ROS 2 image message
-      self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
- 
     # Display the message on the console
-    ##self.get_logger().info('Publishing video frame')
+    self.get_logger().info('Receiving image')
+ 
+    # Convert ROS Image message to OpenCV image
+    current_frame = self.br.imgmsg_to_cv2(data, "bgr8")
     
     # Convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+    
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
     # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
-    cv2.imshow("camera", frame)
+    for (x,y,w,h) in faces:
+        current_frame = cv2.rectangle(current_frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = current_frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    
+    # Display image
+    cv2.imshow("Camera", current_frame)
     
     cv2.waitKey(1)
   
@@ -91,9 +90,6 @@ def main(args=None):
   
   # Shutdown the ROS client library for Python
   rclpy.shutdown()
-  
-  # Release the VideoCapture object
-  cap.release()
   
 if __name__ == '__main__':
   main()
